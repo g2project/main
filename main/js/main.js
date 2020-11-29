4b3f28e674c9db665bab3c9438c178807575cb68
0hoon <kih99ss@naver.com>
2020-11-30 04:49:47 +0900

    메인페이지

--- main/js/main.js
+++ main/js/main.js
@@ -1,103 +1,51 @@
-//current position
-var pos = 0;
-//number of slides
-var totalSlides = $('#slider-wrap ul li').length;
-//get the slide width
-var sliderWidth = $('#slider-wrap').width();
+var slideIndex = 0; //slide index
 
+// HTML 로드가 끝난 후 동작
+window.onload=function(){
+  showSlides(slideIndex);
 
-$(document).ready(function(){
-  
-  
-  /*****************
-   BUILD THE SLIDER
-  *****************/
-  //set width to be 'x' times the number of slides
-  $('#slider-wrap ul#slider').width(sliderWidth*totalSlides);
-  
-    //next slide  
-  $('#next').click(function(){
-    slideRight();
-  });
-  
-  //previous slide
-  $('#previous').click(function(){
-    slideLeft();
-  });
-  
-  
-  
-  /*************************
-   //*> OPTIONAL SETTINGS
-  ************************/
-  //automatic slider
-  var autoSlider = setInterval(slideRight, 3000);
-  
-  //for each slide 
-  $.each($('#slider-wrap ul li'), function() { 
+  // Auto Move Slide
+  var sec = 3000;
+  setInterval(function(){
+    slideIndex++;
+    showSlides(slideIndex);
 
-     //create a pagination
-     var li = document.createElement('li');
-     $('#pagination-wrap ul').append(li);    
-  });
-  
-  //counter
-  countSlides();
-  
-  //pagination
-  pagination();
-  
-  //hide/show controls/btns when hover
-  //pause automatic slide when hover
-  $('#slider-wrap').hover(
-    function(){ $(this).addClass('active'); clearInterval(autoSlider); }, 
-    function(){ $(this).removeClass('active'); autoSlider = setInterval(slideRight, 3000); }
-  );
-  
-  
-
-});//DOCUMENT READY
-  
-
-
-/***********
- SLIDE LEFT
-************/
-function slideLeft(){
-  pos--;
-  if(pos==-1){ pos = totalSlides-1; }
-  $('#slider-wrap ul#slider').css('left', -(sliderWidth*pos));  
-  
-  //*> optional
-  countSlides();
-  pagination();
+  }, sec);
 }
 
 
-/************
- SLIDE RIGHT
-*************/
-function slideRight(){
-  pos++;
-  if(pos==totalSlides){ pos = 0; }
-  $('#slider-wrap ul#slider').css('left', -(sliderWidth*pos)); 
-  
-  //*> optional 
-  countSlides();
-  pagination();
+// Next/previous controls
+function moveSlides(n) {
+  slideIndex = slideIndex + n
+  showSlides(slideIndex);
 }
 
-
-
-  
-/************************
- //*> OPTIONAL SETTINGS
-************************/
-function countSlides(){
-  $('#counter').html(pos+1 + ' / ' + totalSlides);
+// Thumbnail image controls
+function currentSlide(n) {
+  slideIndex = n;
+  showSlides(slideIndex);
 }
 
-function pagination(){
-  $('#pagination-wrap ul li').removeClass('active');
-  $('#pagination-wrap ul li:eq('+pos+')').addClass('active');
+function showSlides(n) {
+
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  var size = slides.length;
+
+  if ((n+1) > size) {
+    slideIndex = 0; n = 0;
+  }else if (n < 0) {
+    slideIndex = (size-1);
+    n = (size-1);
+  }
+
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[n].style.display = "block";
+  dots[n].className += " active";
 }
