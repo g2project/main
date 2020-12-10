740b7268af2f2eb854fa3aa0de28514bc74e5c09
0hoonkim <74031394+0hoonkim@users.noreply.github.com>
2020-12-10 18:36:32 +0900

    zjalt

--- main/js/main.js
+++ main/js/main.js
@@ -29,7 +29,6 @@ function currentSlide(n) {
 function showSlides(n) {
 
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
   var size = slides.length;
 
   if ((n+1) > size) {
@@ -42,12 +41,8 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-
+  
   slides[n].style.display = "block";
-  dots[n].className += " active";
 }
 
 $(document).ready(function(){
