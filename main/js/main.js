78f3281f4633725d41cfcb80dcc6c8c62bc96f4b
0hoonkim <74031394+0hoonkim@users.noreply.github.com>
2020-12-10 17:28:25 +0900

    asdf
    asdf

--- main/js/main.js
+++ main/js/main.js
@@ -49,4 +49,32 @@ function showSlides(n) {
   slides[n].style.display = "block";
   dots[n].className += " active";
 }
- 
+
+$(document).ready(function(){
+  let weatherIcon = {
+      '01' : 'fas fa-sun',
+      '02' : 'fas fa-cloud-sun',
+      '03' : 'fas fa-cloud',
+      '04' : 'fas fa-cloud-meatball',
+      '09' : 'fas fa-cloud-sun-rain',
+      '10' : 'fas fa-cloud-showers-heavy',
+      '11' : 'fas fa-poo-storm',
+      '13' : 'far fa-snowflake',
+      '50' : 'fas fa-smog'
+  };
+
+  $.ajax({
+      url: 'http://api.openweathermap.org/data/2.5/weather?q=Jeju&appid=dab36ab4bb7115dc1e4e1592c7e0d2d4&units=metric',
+      dataType: 'json',
+      type: 'GET',
+      success: function(data){
+          var $Icon = (data.weather[0].icon).substr(0,2);
+          var $Temp = Math.floor(data.main.temp) + 'º';
+          var $city = data.name;
+
+           $('.CurrIcon').append('<i class="' + weatherIcon[$Icon] + '"></i>');
+           $('.CurrTemp').prepend($Temp);
+           $('.City').append($city);
+      }
+  })
+});
