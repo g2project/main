6cf7210b96f8caad536875967b868c69085bcebb
0hoonkim <74031394+0hoonkim@users.noreply.github.com>
2020-12-11 17:21:39 +0900

    asdf
    fsda

--- index2/js/main.js
+++ index2/js/main.js
@@ -45,31 +45,45 @@ function showSlides(n) {
   slides[n].style.display = "block";
 }
 
-$(document).ready(function(){
-  let weatherIcon = {
-      '01' : 'fas fa-sun',
-      '02' : 'fas fa-cloud-sun',
-      '03' : 'fas fa-cloud',
-      '04' : 'fas fa-cloud-meatball',
-      '09' : 'fas fa-cloud-sun-rain',
-      '10' : 'fas fa-cloud-showers-heavy',
-      '11' : 'fas fa-poo-storm',
-      '13' : 'far fa-snowflake',
-      '50' : 'fas fa-smog'
-  };
 
-  $.ajax({
-      url: 'http://api.openweathermap.org/data/2.5/weather?q=Jeju&appid=dab36ab4bb7115dc1e4e1592c7e0d2d4&units=metric',
-      dataType: 'json',
-      type: 'GET',
-      success: function(data){
-          var $Icon = (data.weather[0].icon).substr(0,2);
-          var $Temp = Math.floor(data.main.temp) + 'º';
-          var $city = data.name;
+		// API 요청 URL 변수
+		var url = 'http://api.openweathermap.org/data/2.5/weather?q=jeju&APPID=e11462160015cffa69954c9f67741b7b';
 
-           $('.CurrIcon').append('<i class="' + weatherIcon[$Icon] + '"></i>');
-           $('.CurrTemp').prepend($Temp);
-           $('.City').append($city);
-      }
-  })
-});
+		// 로딩 이미지 표시
+		$('#weather_info .load_img').show();
+
+		$.getJSON(url, function(data){
+
+			// 날씨 데이터 객체
+			var sys = data.sys; 			// 국가명, 일출/일몰			
+			var city = data.name; 			// 도시명
+			var weather = data.weather; 	// 날씨 객체
+			var main = data.main;  			// 온도 기압 관련 객체
+	 
+			var wmain = weather[0].main; 	// 구름 상태(Cloudiness)
+			var w_id = weather[0].id; 		// 날씨상태 id 코드
+			var icon = weather[0].icon;		// 날씨 아이콘 정보
+			var country = sys.country;  	// 국가명
+			var temp = main.temp; 			// 현재 온도
+			var temp_min = main.temp_min 	// 최저 온도
+			var temp_max = main.temp_max 	// 최고 온도
+
+			// 날씨 아이콘
+			var icon_url = 'http://openweathermap.org/img/w/' + icon;
+      
+			// 날씨 정보 표시
+			$('#weather_info > .city').html(city + "/" + country);			
+			$('#weather_info  .icon').html("<img src='" + icon_url + ".png'>");
+			$('#weather_info .w_id').html(wmain);
+			$('#weather_info .temp_min').html(parseInt(temp_min-273.15) + '&deg;' + ' min');
+			$('#weather_info .temp_max').html( parseInt(temp_max-273.15) + '&deg;' + ' max');
+			$('#weather_info  .temp').html(parseInt(temp-273.15) + '&deg;');
+
+			// 데이터 로딩 후 로딩이미지 제거
+			$('#weather_info .load_img').hide();		
+
+		})	// end getJSON()
+			.fail(function() {
+				// 오류 메시지
+   				alert( "loding error" );
+  			});	
